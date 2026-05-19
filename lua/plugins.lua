5549e05d204acbd3516b852dae463817a58e6105
Reach <reach@42paris.fr>
2026-05-19 02:44:54 +0200

    feat(options): de-prioritize .lock/.sum ext

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,4 +86,5 @@ require("blink.cmp").setup({
 	completion = { documentation = { auto_show = false } },
 	sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
 	fuzzy = { implementation = "prefer_rust_with_warning" },
+	cmdline = { enabled = false },
 })
